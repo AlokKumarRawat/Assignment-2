38c5e127f3b1690486d283553cc9cf8c6f21261e
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:25:16 +0530

    Create Question6.c

---
+++ Question6.c
@@ -0,0 +1,11 @@
+// ALOK KUMAR RAWAT
+// ROLL NO 2303510100011
+#include <stdio.h>
+int main() {
+   int a,b,c;
+   printf("Enter three number: ");
+   scanf("%d %d %d",&a,&b,&c);
+   float sum=a+b+c;
+   printf("average is %f",sum/3);
+    return 0;
+}
