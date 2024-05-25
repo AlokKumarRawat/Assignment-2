454566882b75bf4c7f4cf066e61b0d087233cc04
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:33:29 +0530

    Create Question16.c

---
+++ Question16.c
@@ -0,0 +1,19 @@
+// ALOK KUMAR RAWAT
+// 2303510100011
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("Enter the start year: ");
+    scanf("%d",&a);
+    printf("Enter the end year: ");
+    scanf("%d",&b);
+    for(int i=a;i<=b;i++){
+        if(i%4==0){
+            printf("%d\n",i);
+        }
+        else{
+            continue;
+        }
+    }
+    return 0;
+}
