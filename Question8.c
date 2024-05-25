1c7c98ba1b8e45e303421d7dc0387385ceb9dbf2
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:26:15 +0530

    Create Question8.c

---
+++ Question8.c
@@ -0,0 +1,16 @@
+// ALOK KUMAR RAWAT
+// 2303510100011
+#include <stdio.h>
+int main() {
+    int p,t;
+    float r;
+    printf("Enter the principal amount: ");
+    scanf("%d",&p);
+    printf("Enter the rate of intrest: ");
+    scanf("%f",&r);
+    printf("Enter the time in years: ");
+    scanf("%d",&t);
+    float temp=p*r*t;
+    printf("Simple intrest is %f",temp/100);
+    return 0;
+}
