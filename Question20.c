4376005ce7078cded3e24591e7747b5aabcf052d
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:36:04 +0530

    Create Question20.c

---
+++ Question20.c
@@ -0,0 +1,38 @@
+// ALOK KUMAR RAWAT
+// 2303510100011
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number of day : ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("Sunday");
+        break;
+    case 2:
+        printf("Monday");
+        break;
+    case 3:
+        pritnf("Tuesday");
+        break;
+    case 4:
+        printf("Wednesday");
+        break;
+    case 5:
+        printf("Thursday");
+        break;
+    case 6:
+        printf("Friday");
+        break;
+    case 7:
+        printf("Saturday");
+        break;
+    default:
+        printf("Wrong Entry");
+        break;
+    }
+
+    return 0;
+}
