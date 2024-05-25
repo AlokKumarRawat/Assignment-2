35ccd24a8b2c2f365f785ec940b08171bc99fecd
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:21:21 +0530

    Create Question1.c

---
+++ Question1.c
@@ -0,0 +1,15 @@
+// ALOK KUMAR RAWAT
+// ROLL NO 2303510100011
+#include <stdio.h>
+int main() {
+   int a;
+   printf("Enter the year: ");
+   scanf("%d",&a);
+   if(a%4==0){
+       printf("Given year is a leap year");
+   }
+   else{
+       printf("Given year is not a leap year");
+   }
+    return 0;
+}
