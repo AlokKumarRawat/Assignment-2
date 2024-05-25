f8f3742c3e2af50e06c2c5a089062c9a4a5003b5
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:26:53 +0530

    Create Question9.c

---
+++ Question9.c
@@ -0,0 +1,14 @@
+// ALOK KUMAR RAWAT
+// 2303510100011
+#include <stdio.h>
+int main() {
+    int l,b,h;
+    printf("Enter the length of cuboid: ");
+    scanf("%d",&l);
+    printf("Enter the breadth of cuboid: ");
+    scanf("%d",&b);
+    printf("Enter the height of cuboid: ");
+    scanf("%d",&h);
+    printf("the volume of the cuboid id %d",l*b*h);
+    return 0;
+}
