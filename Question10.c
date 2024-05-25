0ead4424a3c0898f34bb1ddb70fedb683b7d6898
Devil-766 <149565273+Devil-766@users.noreply.github.com>
2024-05-25 23:27:48 +0530

    Create Question10.c

---
+++ Question10.c
@@ -0,0 +1,15 @@
+// ALOK KUMAR RAWAT
+// 2303510100011
+#include <stdio.h>
+int main() {
+    char a,d,c;
+    printf("Enter the three characters: ");
+    scanf("%c %c %c",&a,&d,&c);
+    int p=a;
+    int q=d;
+    int r=c;
+    printf("%c----%d\n",a,p);
+    printf("%c----%d\n",d,q);
+    printf("%c----%d\n",c,r);
+    return 0;
+}
